16deca2355fbf6cd24371e902bc743533b79dbf4
Taylor Buckner <tbuckner@theappraisallane.com>
2020-03-14 11:32:51 -0500

    release: build mvp for github pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  petFinder: {
+    id: 'jydZ85I6r81zgjQCvaTjTnB8QBUauslXEeqARU5XWk0ohZludm',
+    secret: 'Zh5UHg0CE4Wpoi8nyy9RdFTe5Xr1WgSaGJ9KBJDv',
+  }
 };
